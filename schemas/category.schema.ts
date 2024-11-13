ab4c7889ee3015cbb83579eb4a6b86b0d42f97c0
Nguyễn Đức Mạnh <work.ducmanh0323@gmail.com>
2024-11-13 15:32:45 +0700

    update category schema

--- schemas/category.schema.ts
+++ schemas/category.schema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 
 export default z.object({
   name: z.string().min(1, 'Please enter category name'),
-  billboardId: z.string().min(1, 'Please select billboard'),
+  billboardId: z.string().uuid().min(1, 'Please select billboard'),
 })
