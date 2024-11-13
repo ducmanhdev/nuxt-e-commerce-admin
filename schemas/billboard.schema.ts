c6e77cab9f79780bc60771aebf4be071d4f33060
Nguyễn Đức Mạnh <work.ducmanh0323@gmail.com>
2024-11-13 15:32:35 +0700

    update billboard schema

--- schemas/billboard.schema.ts
+++ schemas/billboard.schema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 
 export default z.object({
   name: z.string().min(1, 'Please enter billboard name'),
-  imageUrl: z.string().min(1, 'Please enter billboard image'),
+  imageUrl: z.string().url().min(1, 'Please add billboard image'),
 })
