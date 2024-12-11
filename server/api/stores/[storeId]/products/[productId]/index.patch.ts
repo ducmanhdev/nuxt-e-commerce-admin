81de428b63d3bd38698b8d4def5f3ed5d93d0da7
Nguyễn Đức Mạnh <work.ducmanh0323@gmail.com>
2024-12-11 10:46:44 +0700

    update product patch api

--- server/api/stores/[storeId]/products/[productId]/index.patch.ts
+++ server/api/stores/[storeId]/products/[productId]/index.patch.ts
@@ -5,6 +5,7 @@ export default defineWrappedResponseHandler(async (event) => {
   const user = event.context.user
 
   const { storeId, productId } = getRouterParams(event)
+
   const product = await prisma.product.findFirstOrThrow({
     where: {
       id: productId,
@@ -15,14 +16,84 @@ export default defineWrappedResponseHandler(async (event) => {
     },
   })
 
-  const body = await readValidatedBody(event, schema.parse)
-  const data = await prisma.product.update({
-    where: {
-      id: product.id,
-    },
-    data: {
-      ...body,
-    },
+  const { attributes, ...body } = await readValidatedBody(event, schema.parse)
+
+  const data = await prisma.$transaction(async (tx) => {
+    const updatedProduct = await tx.product.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        ...body,
+      },
+    })
+
+    if (attributes?.length) {
+      for (const attr of attributes) {
+        let attribute = await tx.attribute.findFirst({
+          where: {
+            name: attr.name,
+            storeId: storeId,
+          },
+        })
+
+        if (!attribute) {
+          attribute = await tx.attribute.create({
+            data: {
+              name: attr.name,
+              storeId: storeId,
+            },
+          })
+        }
+
+        const productAttributeValue = await tx.productAttributeValue.findFirst({
+          where: {
+            productId: product.id,
+            attributeId: attribute.id,
+          },
+        })
+
+        if (productAttributeValue) {
+          await tx.productAttributeValue.update({
+            where: {
+              id: productAttributeValue.id,
+            },
+            data: {
+              value: attr.value,
+            },
+          })
+        } else {
+          await tx.productAttributeValue.create({
+            data: {
+              productId: product.id,
+              attributeId: attribute.id,
+              value: attr.value,
+            },
+          })
+        }
+      }
+
+      const existingAttributes = await tx.productAttributeValue.findMany({
+        where: {
+          productId: product.id,
+        },
+        include: {
+          attribute: true,
+        },
+      })
+
+      await tx.productAttributeValue.deleteMany({
+        where: {
+          id: {
+            in: existingAttributes
+              .filter((existingAttr) => !attributes.find((attr) => attr.name === existingAttr.attribute.name))
+              .map((existingAttr) => existingAttr.id),
+          },
+        },
+      })
+    }
+
+    return updatedProduct
   })
 
   return {
